80ac13e45e5520e11248eb3832fcde2ef387197c
anissa111 <anissaz@ucar.edu>
2021-12-16 19:31:03 -0700

    complete day3

--- 2021/day3.py
+++ 2021/day3.py
@@ -2,18 +2,10 @@ import numpy as np
 from aocd.models import Puzzle
 
 
-def part1(raw):
-
-    # convert bits to individual ints
-    for i in range(len(raw)):
-        raw[i] = [int(a) for a in str(raw[i])]
-
-    # convert to a numpy array and transpose
-    raw = np.asarray(raw).T
-
+def part1(input):
     gamma_str = ''
-    for row in raw:
-        b = np.bincount(row).argmax()
+    for i in range(len(input[0])):
+        b = most_common_bit(input, i)
         gamma_str = gamma_str + str(b)
 
     # convert binary string to int
@@ -28,18 +20,70 @@ def part1(raw):
     return gamma * epsilon
 
 
+def part2(input):
+    inputmin = np.array(input)
+    inputmax = np.array(input)
+
+    # for every position
+    for i in range(len(input[0])):
+        bmax = most_common_bit(inputmax, i)
+
+        inputmax = inputmax[np.where(np.transpose(inputmax)[i] == bmax)]
+
+        if len(inputmax) == 1:
+            break
+
+    for i in range(len(input[0])):
+        bmin = least_common_bit(inputmin, i)
+
+        inputmin = inputmin[np.where(np.transpose(inputmin)[i] == bmin)]
+
+        if len(inputmin) == 1:
+            break
+
+    oxygen = int("".join(str(x) for x in inputmax[0]), 2)
+    co2 = int("".join(str(x) for x in inputmin[0]), 2)
+
+    return oxygen * co2
+
+
+def most_common_bit(input, position):
+    # transpose
+    inputT = np.transpose(np.asarray(input))
+    b = np.bincount(inputT[position])
+
+    if b[0] == b[1]:
+        return 1
+
+    return b.argmax()
+
+
+def least_common_bit(input, position):
+    # transpose
+    inputT = np.transpose(np.asarray(input))
+    b = np.bincount(inputT[position])
+
+    if b[0] == b[1]:
+        return 0
+
+    return 1 - b.argmax()
+
+
 if __name__ == '__main__':
     # import unique puzzle data
     puzzle = Puzzle(2021, 3)
 
     # clean data
     raw = puzzle.input_data.split('\n')
+    # convert bits to individual ints
+    for i in range(len(raw)):
+        raw[i] = [int(a) for a in str(raw[i])]
 
     # run part 1
     answer1 = part1(raw)
-    #
-    # # run part 2
-    # answer2 = part2(raw)
-    #
+
+    # run part 2
+    answer2 = part2(raw)
+
     print(f'Part 1: {answer1}')
-    # print(f'Part 2: {answer2}')
+    print(f'Part 2: {answer2}')
