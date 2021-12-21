7365015a56aa20b48b4d1a875f049ebec3d0652b
anissa111 <anissaz@ucar.edu>
2021-12-21 16:04:50 -0700

    2021-06 complete

--- 2021/day6.py
+++ 2021/day6.py
@@ -4,26 +4,24 @@ from aocd.models import Puzzle
 
 def lifecycle(lf, days):
 
-    # print(f'After 0 days: {lf}')
+    pop = len(lf)
 
-    for i in range(days):
+    spawn = np.zeros(days)
 
-        # spawn new fish with an extra day
-        lf = np.append(lf, np.ones(len(lf[lf == 0])) * 9)
+    # populate spawn
+    for f in lf:
+        spawn[f] = spawn[f] + 1
 
-        # decrease days for all fish
-        lf = lf - 1
+    # propagate initialized spawn
+    for i in range(days)[:-7]:
+        if i+9 < days:
+            spawn[i + 9] = spawn[i + 9] + spawn[i]
+        spawn[i + 7] = spawn[i + 7] + spawn[i]
 
-        # cycle fist that have completed spawn
-        lf[lf == -1] = 6
+    # calculate total fish
+    pop = pop + sum(spawn)
 
-
-        # print(f'After {i+1} days: {lf}')
-
-    return len(lf)
-
-def calc_children(lf, days)
-
+    return pop
 
 
 if __name__ == '__main__':
@@ -31,7 +29,6 @@ if __name__ == '__main__':
     puzzle = Puzzle(2021, 6)
 
     input = np.array(list((map(int, puzzle.input_data.split(',')))))
-    days = 80
 
     test_input = np.array([3, 4, 3, 1, 2])
 
