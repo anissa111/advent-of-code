4f0bf0d50684b9fb5ca50b2e1eaa66b2e2e01726
anissa111 <anissaz@ucar.edu>
2022-02-12 16:48:24 -0700

    complete 2015 day 2

--- 2015/day2.py
+++ 2015/day2.py
@@ -10,6 +10,26 @@ def part1(data):
     
     return yardage
 
+def part2(data):
+
+    ribbon = 0
+
+    # for every package, calculate ribbon length and add to total
+    for l in data:
+        ribbon += calc_ribbon(l[0],l[1],l[2])
+    
+    return ribbon
+
+
+def calc_ribbon(x,y,z):
+    
+    # find perimeter of smallest side (presorted)
+        ribbon = 2*x + 2*y
+
+    # add volume for bow
+        ribbon += x*y*z
+        
+        return ribbon
 
 def calc_yardage(x,y,z):
     # find yardage and add length of smallest side
@@ -18,8 +38,12 @@ def calc_yardage(x,y,z):
 
 def parse(raw):
     data = []
+
+    # for every line, parse, sort, and add to data
     for l in raw.split('\n'):
-        data.append([int(x) for x in l.split('x')])
+        next = [int(x) for x in l.split('x')]
+        next.sort()
+        data.append(next)
     
     return data
 
@@ -33,5 +57,7 @@ if __name__ == '__main__':
     data = parse(raw)
 
     answer1 = part1(data)
+    answer2 = part2(data)
 
     print(f'Part 1: {answer1}')
+    print(f'Part 2: {answer2}')
