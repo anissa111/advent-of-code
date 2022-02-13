7232d7816f63b812d26fb84969981ac15ed4f96a
anissa111 <anissaz@ucar.edu>
2022-02-12 20:24:52 -0700

    completed 2015 day 4

---
+++ 2015/day4.py
@@ -0,0 +1,28 @@
+from aocd.models import Puzzle
+from hashlib import md5
+
+def find_hash(pref, key):
+    key = key.encode('ascii')
+    suffix = 1
+
+    # loop until we find a hash that starts with the prefix
+    while True:
+        hash = md5(key + str(suffix).encode('ascii')).hexdigest()
+
+        if hash.startswith(pref):
+            return suffix
+        
+        suffix += 1
+
+
+if __name__ == '__main__':
+
+    # import unique puzzle data
+    puzzle = Puzzle(2015, 4)
+    secret_key = puzzle.input_data
+
+    answer1 = find_hash('00000', secret_key)
+    answer2 = find_hash('000000', secret_key)
+
+    print(f'Part 1: {answer1}')
+    print(f'Part 2: {answer2}')
