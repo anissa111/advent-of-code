fd1eb000f0940769e2d79f4ab53ada4cf7352bfe
anissa111 <anissaz@ucar.edu>
2022-02-13 12:46:16 -0700

    completed 2015 day 6

--- 2015/day6.py
+++ 2015/day6.py
@@ -45,7 +45,50 @@ def part1(raw):
     # tally up the number of lights on
     return np.sum(lights)
 
+
+
+def part2(raw):
+
+    # set up empty lights array
+    lights = np.zeros((1000,1000), dtype=int)
+
+    # loop through instructions
+    for l in raw.split('\n'):
+        l = l.split()
+
+         # if instruction is toggle
+        if l[0] == 'toggle':
             
+            # get start and end coordinates
+            start = [int(x) for x in l[1].split(',')]
+            end = [int(x) for x in l[3].split(',')]
+
+            # create a "sector" of lights
+            sector = (slice(start[1], end[1]+1), slice(start[0], end[0]+1))
+
+            # increase brightness of lights in range by 2
+            lights[sector] += 2
+        
+        # if instruction is turn on or off
+        else:
+            # get start and end coordinates
+            start = [int(x) for x in l[2].split(',')]
+            end = [int(x) for x in l[4].split(',')]
+
+            # create a "sector" of lights
+            sector = (slice(start[1], end[1]+1), slice(start[0], end[0]+1))
+
+            # if instruction is turn on
+            if l[1] == 'on':
+                # increase brightness by 1
+                lights[sector] += 1
+            # if instruction is turn off
+            else:
+                # decrease brightness by 1 to a minimum of 0
+                lights[sector] = np.maximum(lights[sector]-1, 0)
+
+    #  tally up the light brightness
+    return np.sum(lights)           
 
 
 if __name__ == '__main__':
@@ -54,5 +97,7 @@ if __name__ == '__main__':
     raw = puzzle.input_data
 
     answer1 = part1(raw)
+    answer2 = part2(raw)
 
     print(f'Part 1: {answer1}')
+    print(f'Part 2: {answer2}')
