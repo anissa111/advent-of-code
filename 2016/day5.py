0bc7c4c0f7788157d7e132490929cb59daf11ffb
anissa111 <anissaz@ucar.edu>
2023-10-21 16:14:18 -0600

    2016 day 5

--- 2016/day5.py
+++ 2016/day5.py
@@ -2,11 +2,12 @@ import hashlib
 
 raw = 'ffykfhsq'
 
+
 def get_password_1(raw):
     i = 0
     password = ''
-    while len(password)<8:
-        md5hash = hashlib.md5((raw+str(i)).encode()).hexdigest()
+    while len(password) < 8:
+        md5hash = hashlib.md5((raw + str(i)).encode()).hexdigest()
         if md5hash[:5] == '00000':
             password = password + md5hash[5]
 
@@ -14,12 +15,13 @@ def get_password_1(raw):
 
     return password
 
+
 def get_password_2(raw):
     i = 0
     found = [False, False, False, False, False, False, False, False]
     password = [0, 0, 0, 0, 0, 0, 0, 0]
     while not all(found):
-        md5hash = hashlib.md5((raw+str(i)).encode()).hexdigest()
+        md5hash = hashlib.md5((raw + str(i)).encode()).hexdigest()
         if md5hash[:5] == '00000' and md5hash[5].isdigit():
             if int(md5hash[5]) < 8 and not found[int(md5hash[5])]:
                 password[int(md5hash[5])] = md5hash[6]
@@ -27,6 +29,7 @@ def get_password_2(raw):
         i += 1
 
     return ''.join(password)
+
 
 print(f'Part 1: {get_password_1(raw)}')
 print(f'Part 2: {get_password_2(raw)}')
