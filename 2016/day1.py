cc4230e35342596dd5ae9aa4e45becfbcefd4a58
anissa111 <anissaz@ucar.edu>
2023-10-20 21:59:08 -0600

    2016 day 1 pt 2

--- 2016/day1.py
+++ 2016/day1.py
@@ -5,8 +5,10 @@ import numpy as np
 puzzle = Puzzle(2016, 1)
 raw = puzzle.input_data.split(', ')
 
-# example data
+# shorter/example data
 #raw = "R5, L5, R5, R3".split(', ')
+#raw = "R8, R4, R4, R8".split(", ")
+#raw = raw[:10]
 
 # rotational
 rotate = {
@@ -15,11 +17,30 @@ rotate = {
 }
 
 # start pointing north at [0,0]
-direction = [0, 1]
-location = [0, 0]
+direction = np.array([0, 1])
+location = np.array([0, 0])
+history = set([tuple(location)])
+found = False
+
+print(history)
+
 
 for step in raw:
+    dist = int(step[1:])
     direction = np.dot(direction, rotate[step[0]])
-    location = location + direction*int(step[1:])
+
+    # add all steps to memory
+    if not found:
+        for i in range(dist):
+            s = location + direction*(i+1)
+            if tuple(s) in history:
+                print("Part 2: " + str(sum(abs(s))))
+                found = True
+            else:
+                history.add(tuple(s))
+
+
+    # update location
+    location = location + direction*dist
 
 print("Part 1: " + str(sum(abs(location))))
