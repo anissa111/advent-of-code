71f2b7498be2f4a1dce065f06be0572d0f7e9287
anissa111 <anissaz@ucar.edu>
2023-10-20 22:50:25 -0600

    precommits

--- 2016/day1.py
+++ 2016/day1.py
@@ -1,20 +1,17 @@
 from aocd.models import Puzzle
 import numpy as np
 
- # import unique puzzle data
+# import unique puzzle data
 puzzle = Puzzle(2016, 1)
-raw = puzzle.input_data.split(', ')
+raw = puzzle.input_data.split(", ")
 
 # shorter/example data
-#raw = "R5, L5, R5, R3".split(', ')
-#raw = "R8, R4, R4, R8".split(", ")
-#raw = raw[:10]
+# raw = "R5, L5, R5, R3".split(', ')
+# raw = "R8, R4, R4, R8".split(", ")
+# raw = raw[:10]
 
 # rotational
-rotate = {
-    "L": np.array([[0, 1], [-1, 0]]),
-    "R": np.array([[0, -1], [1, 0]])
-}
+rotate = {"L": np.array([[0, 1], [-1, 0]]), "R": np.array([[0, -1], [1, 0]])}
 
 # start pointing north at [0,0]
 direction = np.array([0, 1])
@@ -32,15 +29,14 @@ for step in raw:
     # add all steps to memory
     if not found:
         for i in range(dist):
-            s = location + direction*(i+1)
+            s = location + direction * (i + 1)
             if tuple(s) in history:
                 print("Part 2: " + str(sum(abs(s))))
                 found = True
             else:
                 history.add(tuple(s))
 
-
     # update location
-    location = location + direction*dist
+    location = location + direction * dist
 
 print("Part 1: " + str(sum(abs(location))))
--- 2016/day2.py
+++ 2016/day2.py
@@ -1,15 +1,17 @@
 from aocd.models import Puzzle
 import numpy as np
 
+
 def decode(start, keypad, directions, subs={}):
     curr = start
     for d in directions:
-        curr = keypad[d][curr-1]
+        curr = keypad[d][curr - 1]
 
     if curr in subs:
         curr = subs[curr]
 
     return str(curr)
+
 
 # import unique puzzle data
 puzzle = Puzzle(2016, 2)
@@ -20,10 +22,10 @@ raw = puzzle.input_data.split()
 # 7 8 9
 
 keypad1 = {
-    'U': [1,2,3,1,2,3,4,5,6],
-    'D': [4,5,6,7,8,9,7,8,9],
-    'L': [1,1,2,4,4,5,7,7,8],
-    'R': [2,3,3,5,6,6,8,9,9]
+    "U": [1, 2, 3, 1, 2, 3, 4, 5, 6],
+    "D": [4, 5, 6, 7, 8, 9, 7, 8, 9],
+    "L": [1, 1, 2, 4, 4, 5, 7, 7, 8],
+    "R": [2, 3, 3, 5, 6, 6, 8, 9, 9],
 }
 
 #     1
@@ -34,18 +36,17 @@ keypad1 = {
 
 a, b, c, d = (10, 11, 12, 13)
 keypad2 = {
-    'U': [1,2,1,4,5,2,3,4,9,6,7,8,b],
-    'D': [2,6,7,8,5,a,b,c,9,a,d,c,d],
-    'L': [1,2,2,3,5,5,6,7,8,a,a,b,d],
-    'R': [1,3,4,4,6,7,8,9,9,b,c,c,d]
+    "U": [1, 2, 1, 4, 5, 2, 3, 4, 9, 6, 7, 8, b],
+    "D": [2, 6, 7, 8, 5, a, b, c, 9, a, d, c, d],
+    "L": [1, 2, 2, 3, 5, 5, 6, 7, 8, a, a, b, d],
+    "R": [1, 3, 4, 4, 6, 7, 8, 9, 9, b, c, c, d],
 }
-subs = {10:'a', 11:'b', 12:'c', 13:'d'}
+subs = {10: "a", 11: "b", 12: "c", 13: "d"}
 
 start = 5
 
-code1 = int(''.join([decode(start, keypad1, line) for line in raw]))
+code1 = int("".join([decode(start, keypad1, line) for line in raw]))
 print("Part 1: " + str(code1))
 
-code2 =''.join([decode(start, keypad2, line, subs) for line in raw])
+code2 = "".join([decode(start, keypad2, line, subs) for line in raw])
 print("Part 2: " + str(code2))
-
