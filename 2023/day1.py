d1be51dc5d4d5a49cabedc20acd40728edc468f7
anissa111 <anissaz@ucar.edu>
2023-12-01 11:36:06 -0700

    a little morning clean up

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,5 +1,4 @@
 from aocd.models import Puzzle
-import numpy as np
 
 puzzle = Puzzle(2023,1).input_data.split('\n')
 
@@ -15,44 +14,30 @@ str_nums = {
     'nine': '9',
 }
 
+overlaps = {
+    'oneight': 'oneeight',
+    'threeignt': 'threeeight',
+    'fiveignt': 'fiveeight',
+    'nineight': 'nineeight',
+    'twone': 'twoone',
+    'sevenine': 'sevennine',
+    'eightwo': 'eighttwo',
+}
+
 calibration_sum_1 = 0
 calibration_sum_2 = 0
+
 for l in puzzle:
     l_1 = [d for d in l if d.isdigit()]
     calibration_sum_1 += int(''.join([l_1[0], l_1[-1]]))
 
-for l in puzzle:
-    partial = ''
-    first = None
-    last = None
-
-    partial = ''
-    while first is None:
-        for c in l:
-            if c.isdigit() and first is None:
-                first = c
-            partial = partial + c
-            str_check = [n in partial for n in str_nums]
-            if any(str_check) and first is None:
-                first = str(str_check.index(True)+1)
-
-    while last is None:
-        for c in l[::-1]:
-            if c.isdigit() and last is None:
-                last = c
-            partial = c + partial
-            str_check = [n in partial for n in str_nums]
-            if any(str_check) and last is None:
-                last = str(str_check.index(True)+1)
-
-
-
-
-
-    calibration_sum_2 += int(''.join([first, last]))
+    for o in overlaps:
+        l = l.replace(o, overlaps[o])
+    for n in str_nums:
+        l = l.replace(n, str_nums[n])
+    l_2 = [d for d in l if d.isdigit()]
+    calibration_sum_2 += int(''.join([l_2[0], l_2[-1]]))
 
 
 print(f'Part 1: {calibration_sum_1}')
 print(f'Part 2: {calibration_sum_2}')
-
-
