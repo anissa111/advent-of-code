af4c549ef0cba145c4ad0e4f82b48a8af074eb2f
anissa111 <anissaz@ucar.edu>
2023-12-01 23:04:57 -0700

    a better 2023 day 2

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,41 +1,28 @@
 from aocd.models import Puzzle
 import numpy as np
 import re
+from math import prod
 
 puzzle = Puzzle(2023,2).input_data.split('\n')
 #puzzle = Puzzle(2023,2).examples[0].input_data.split('\n')
 
-rmax = 12
-gmax = 13
-bmax = 14
+totalmax = {
+    'red': 12,
+    'green': 13,
+    'blue': 14,
+}
 
 possible_sum = 0
 power_sum = 0
 
 for gn in range(len(puzzle)):
-    matches = puzzle[gn].split(':')[1].split(';')
-    possible = True
-    grmax = 0
-    ggmax = 0
-    gbmax = 0
 
-    for m in matches:
-        cubes = m.split(',')
-        for c in cubes:
-            match re.split(r' ', c)[1:]:
-                case [n, 'blue']:
-                    if int(n)>bmax: possible=False
-                    if int(n)>gbmax: gbmax=int(n)
-                case [n, 'green']:
-                    if int(n)>gmax: possible=False
-                    if int(n)>ggmax: ggmax=int(n)
-                case [n, 'red']:
-                    if int(n)>rmax: possible=False
-                    if int(n)>grmax: grmax=int(n)
+    counts = [list(map(int, re.findall(rf'(\d+) {color}', puzzle[gn]))) for color in totalmax]
+    max_counts = [max(c) for c in counts]
+    power_sum += prod(max_counts)
 
-    power_sum += grmax*ggmax*gbmax
-
-    if possible: possible_sum += gn + 1
+    if all([game<=total for game, total in zip(max_counts, list(totalmax.values()))]):
+        possible_sum += gn + 1
 
 print(f'Part 1: {possible_sum}')
 print(f'Part 2: {power_sum}')
