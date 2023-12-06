007ee420ad797fb355905ea7bf548cb4ed089727
anissa111 <anissaz@ucar.edu>
2023-12-06 14:21:10 -0700

    2023 day 6 + comments

--- 2023/day6.py
+++ 2023/day6.py
@@ -10,9 +10,11 @@ puzzle = Puzzle(2023, 6)
 data = puzzle.input_data.split('\n')
 #data = puzzle.examples[0].input_data.split('\n')
 
+# get pairs of t and d from input
 tmax = [int(n) for n in re.findall(r'\d+', data[0])]
 dmin = [int(n) for n in re.findall(r'\d+', data[1])]
 
+# get the number of ways for each using quadratic eq and multiply together
 nways=1
 for t, d in zip(tmax, dmin):
     t1 = floor((t - sqrt(t*t - 4*d))/2)
@@ -21,9 +23,11 @@ for t, d in zip(tmax, dmin):
 
 print(f'Part 1 {nways}')
 
+# squish all the numbers together
 t = int(''.join(re.findall(r'\d+', data[0])))
 d = int(''.join(re.findall(r'\d+', data[1])))
 
+# another quadratic equation
 t1 = floor((t - sqrt(t*t - 4*d))/2)
 t2 = ceil((t + sqrt(t*t - 4*d))/2)
 print(f'Part 1: {t2-t1-1}')
